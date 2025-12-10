298cf9057e2cb3596eb15b5b5c680668717919a6
Fivjejinek <andyinventor@outlook.com>
2025-12-10 13:18:51 -0800

    Update ArduinoPinsDataModel.cs

--- src/Artemis.Plugins.SerialInput/ArduinoPinsDataModel.cs
+++ src/Artemis.Plugins.SerialInput/ArduinoPinsDataModel.cs
@@ -1,44 +1,20 @@
 using Artemis.Core.Modules;
-using Artemis.Core.Models;
 
-namespace ArduinoPinsPlugin
+namespace Artemis.Plugins.SerialInput
 {
     public class ArduinoPinsDataModel : DataModel
     {
-        [DataModelProperty(Name = "Pin 2 State")]
         public bool Pin2 { get; set; }
-
-        [DataModelProperty(Name = "Pin 3 State")]
         public bool Pin3 { get; set; }
-
-        [DataModelProperty(Name = "Pin 4 State")]
         public bool Pin4 { get; set; }
-
-        [DataModelProperty(Name = "Pin 5 State")]
         public bool Pin5 { get; set; }
-
-        [DataModelProperty(Name = "Pin 6 State")]
         public bool Pin6 { get; set; }
-
-        [DataModelProperty(Name = "Pin 7 State")]
         public bool Pin7 { get; set; }
-
-        [DataModelProperty(Name = "Pin 8 State")]
         public bool Pin8 { get; set; }
-
-        [DataModelProperty(Name = "Pin 9 State")]
         public bool Pin9 { get; set; }
-
-        [DataModelProperty(Name = "Pin 10 State")]
         public bool Pin10 { get; set; }
-
-        [DataModelProperty(Name = "Pin 11 State")]
         public bool Pin11 { get; set; }
-
-        [DataModelProperty(Name = "Pin 12 State")]
         public bool Pin12 { get; set; }
-
-        [DataModelProperty(Name = "Pin 13 State")]
         public bool Pin13 { get; set; }
     }
 }
