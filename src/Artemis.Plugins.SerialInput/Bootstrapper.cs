69cacb1facdded12ec2f2a0959eae5e75de9c121
Fivjejinek <andyinventor@outlook.com>
2025-12-10 15:16:01 -0800

    Update Bootstrapper.cs

--- src/Artemis.Plugins.SerialInput/Bootstrapper.cs
+++ src/Artemis.Plugins.SerialInput/Bootstrapper.cs
@@ -1,22 +1,21 @@
 using Artemis.Core;
+using Artemis.UI.Shared;
+using Artemis.Plugins.SerialInput.Views;
+using Artemis.Plugins.SerialInput.ViewModels;
 
-namespace Artemis.Plugins.SerialInput
+namespace Artemis.Plugins.SerialInput;
+
+public class Bootstrapper : PluginBootstrapper
 {
-    public class Bootstrapper : PluginBootstrapper
+    public override void OnPluginLoaded(Plugin plugin)
     {
-        public override void OnPluginLoaded(Plugin plugin)
-        {
-            // Nothing required here for settings; the module will create/read them via PluginSettings.
-        }
-
-        public override void OnPluginEnabled(Plugin plugin)
-        {
-            // Register the module with the plugin host.
-            plugin.Register<ArduinoPinsModule>();
-        }
-
-        public override void OnPluginDisabled(Plugin plugin)
-        {
-        }
+        plugin.ConfigurationDialog = new PluginConfigurationDialog<SerialInputConfigurationView, SerialInputConfigurationViewModel>();
     }
+
+    public override void OnPluginEnabled(Plugin plugin)
+    {
+        plugin.Register<ArduinoPinsModule>();
+    }
+
+    public override void OnPluginDisabled(Plugin plugin) { }
 }
