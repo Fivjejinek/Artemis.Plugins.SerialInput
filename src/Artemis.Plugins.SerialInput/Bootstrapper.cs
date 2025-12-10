effa23dd993f427b8ef3760f21a4e521d6d524a2
Fivjejinek <andyinventor@outlook.com>
2025-12-10 13:46:05 -0800

    Update Bootstrapper.cs

--- src/Artemis.Plugins.SerialInput/Bootstrapper.cs
+++ src/Artemis.Plugins.SerialInput/Bootstrapper.cs
@@ -5,21 +5,21 @@ namespace Artemis.Plugins.SerialInput
 {
     public class Bootstrapper : PluginBootstrapper
     {
-        private PluginSetting<string>? _comPort;
-        private PluginSetting<int>? _baudRate;
+        private PluginSetting<string> _comPort = null!;
+        private PluginSetting<int> _baudRate = null!;
 
         public override void OnPluginLoaded(Plugin plugin)
         {
-            // Create settings directly from the bootstrapper
-            _comPort = plugin.CreateSetting("ComPort", "COM3");
-            _baudRate = plugin.CreateSetting("BaudRate", 9600);
+            // Create settings using the bootstrapper’s CreateSetting<T>
+            _comPort = CreateSetting("ComPort", "COM3");
+            _baudRate = CreateSetting("BaudRate", 9600);
         }
 
         public override void OnPluginEnabled(Plugin plugin)
         {
             // Register your module with the plugin
-            plugin.RegisterModule<ArduinoPinsModule>(() =>
-                new ArduinoPinsModule(_comPort?.Value ?? "COM3", _baudRate?.Value ?? 9600));
+            plugin.Register<ArduinoPinsModule>(() =>
+                new ArduinoPinsModule(_comPort.Value, _baudRate.Value));
         }
 
         public override void OnPluginDisabled(Plugin plugin)
