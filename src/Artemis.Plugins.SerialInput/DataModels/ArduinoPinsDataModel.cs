38e8c483c80238c1d3b2ffa7a768addaa6e3524e
Fivjejinek <andyinventor@outlook.com>
2025-12-10 15:34:04 -0800

    Update ArduinoPinsDataModel.cs

--- src/Artemis.Plugins.SerialInput/DataModels/ArduinoPinsDataModel.cs
+++ src/Artemis.Plugins.SerialInput/DataModels/ArduinoPinsDataModel.cs
@@ -4,6 +4,9 @@ namespace Artemis.Plugins.SerialInput;
 
 public class ArduinoPinsDataModel : DataModel
 {
+    // Digital pins D0–D53
+    public bool Pin0 { get; set; }
+    public bool Pin1 { get; set; }
     public bool Pin2 { get; set; }
     public bool Pin3 { get; set; }
     public bool Pin4 { get; set; }
@@ -16,4 +19,62 @@ public class ArduinoPinsDataModel : DataModel
     public bool Pin11 { get; set; }
     public bool Pin12 { get; set; }
     public bool Pin13 { get; set; }
+    public bool Pin14 { get; set; }
+    public bool Pin15 { get; set; }
+    public bool Pin16 { get; set; }
+    public bool Pin17 { get; set; }
+    public bool Pin18 { get; set; }
+    public bool Pin19 { get; set; }
+    public bool Pin20 { get; set; }
+    public bool Pin21 { get; set; }
+    public bool Pin22 { get; set; }
+    public bool Pin23 { get; set; }
+    public bool Pin24 { get; set; }
+    public bool Pin25 { get; set; }
+    public bool Pin26 { get; set; }
+    public bool Pin27 { get; set; }
+    public bool Pin28 { get; set; }
+    public bool Pin29 { get; set; }
+    public bool Pin30 { get; set; }
+    public bool Pin31 { get; set; }
+    public bool Pin32 { get; set; }
+    public bool Pin33 { get; set; }
+    public bool Pin34 { get; set; }
+    public bool Pin35 { get; set; }
+    public bool Pin36 { get; set; }
+    public bool Pin37 { get; set; }
+    public bool Pin38 { get; set; }
+    public bool Pin39 { get; set; }
+    public bool Pin40 { get; set; }
+    public bool Pin41 { get; set; }
+    public bool Pin42 { get; set; }
+    public bool Pin43 { get; set; }
+    public bool Pin44 { get; set; }
+    public bool Pin45 { get; set; }
+    public bool Pin46 { get; set; }
+    public bool Pin47 { get; set; }
+    public bool Pin48 { get; set; }
+    public bool Pin49 { get; set; }
+    public bool Pin50 { get; set; }
+    public bool Pin51 { get; set; }
+    public bool Pin52 { get; set; }
+    public bool Pin53 { get; set; }
+
+    // Analog pins A0–A15
+    public int A0 { get; set; }
+    public int A1 { get; set; }
+    public int A2 { get; set; }
+    public int A3 { get; set; }
+    public int A4 { get; set; }
+    public int A5 { get; set; }
+    public int A6 { get; set; }
+    public int A7 { get; set; }
+    public int A8 { get; set; }
+    public int A9 { get; set; }
+    public int A10 { get; set; }
+    public int A11 { get; set; }
+    public int A12 { get; set; }
+    public int A13 { get; set; }
+    public int A14 { get; set; }
+    public int A15 { get; set; }
 }
