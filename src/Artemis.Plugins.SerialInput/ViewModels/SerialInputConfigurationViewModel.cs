489c42c3133d6b45043a32fc071b105474c1a924
Fivjejinek <andyinventor@outlook.com>
2025-12-10 17:18:16 -0800

    Update SerialInputConfigurationViewModel.cs

--- src/Artemis.Plugins.SerialInput/ViewModels/SerialInputConfigurationViewModel.cs
+++ src/Artemis.Plugins.SerialInput/ViewModels/SerialInputConfigurationViewModel.cs
@@ -7,12 +7,14 @@ namespace Artemis.Plugins.SerialInput.ViewModels
     {
         public PluginSetting<string> ComPort { get; }
         public PluginSetting<int> BaudRate { get; }
+        public PluginSetting<double> UpdateRate { get; }
 
         public SerialInputConfigurationViewModel(Plugin plugin, PluginSettings settings)
             : base(plugin)
         {
-            ComPort = settings.GetSetting("ComPort", "COM3");
-            BaudRate = settings.GetSetting("BaudRate", 115200);
+            ComPort = settings.GetSetting("ComPort", "COM11");       // default COM11
+            BaudRate = settings.GetSetting("BaudRate", 115200);      // default 115200
+            UpdateRate = settings.GetSetting("UpdateRate", 0.1);     // default 0.1s
         }
     }
 }
