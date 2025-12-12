354a7c12e3382ea2d0869303d0b1a05e53e4c53f
Fivjejinek <andyinventor@outlook.com>
2025-12-12 07:44:41 -0800

    Update SerialInputConfigurationViewModel.cs

--- src/Artemis.Plugins.SerialInput/ViewModels/SerialInputConfigurationViewModel.cs
+++ src/Artemis.Plugins.SerialInput/ViewModels/SerialInputConfigurationViewModel.cs
@@ -3,7 +3,7 @@ using Artemis.Core.Modules;
 using Artemis.UI.Shared;
 namespace Artemis.Plugins.SerialInput.ViewModels
 {
-    public class SerialInputConfigurationViewModel : ModuleViewModel
+    public class SerialInputConfigurationViewModel : PluginConfigurationViewModel
     {
         public PluginSetting<string> ComPort { get; }
         public PluginSetting<int> BaudRate { get; }
