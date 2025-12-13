72f201ef15747c012490d937817f137c05042d3d
Fivjejinek <andyinventor@outlook.com>
2025-12-13 08:34:58 -0800

    Update SerialInputConfigurationViewModel.cs

--- src/Artemis.Plugins.SerialInput/ViewModels/SerialInputConfigurationViewModel.cs
+++ src/Artemis.Plugins.SerialInput/ViewModels/SerialInputConfigurationViewModel.cs
@@ -1,18 +1,52 @@
+using System.Reactive;
 using Artemis.Core;
 using Artemis.Core.Modules;
 using Artemis.UI.Shared;
+using ReactiveUI;
+
 namespace Artemis.Plugins.SerialInput.ViewModels
 {
     public class SerialInputConfigurationViewModel : PluginConfigurationViewModel
     {
-        public PluginSetting<string> ComPort { get; }
-        public PluginSetting<int> BaudRate { get; }
+        private readonly PluginSetting<string> _comPort;
+        private readonly PluginSetting<int> _baudRate;
 
         public SerialInputConfigurationViewModel(Plugin plugin, PluginSettings settings)
             : base(plugin)
         {
-            ComPort = settings.GetSetting("ComPort", string.Empty);
-            BaudRate = settings.GetSetting("BaudRate", 0);
+            _comPort = settings.GetSetting("ComPort", string.Empty);
+            _baudRate = settings.GetSetting("BaudRate", 250000);
+
+            ComPort = _comPort.Value;
+            BaudRate = _baudRate.Value;
+
+            SaveChanges = ReactiveCommand.Create(ExecuteSaveChanges);
+            Cancel = ReactiveCommand.Create(ExecuteCancel);
+        }
+
+        public string ComPort { get; set; }
+        public int BaudRate { get; set; }
+
+        public ReactiveCommand<Unit, Unit> SaveChanges { get; }
+        public ReactiveCommand<Unit, Unit> Cancel { get; }
+
+        private void ExecuteSaveChanges()
+        {
+            _comPort.Value = ComPort;
+            _baudRate.Value = BaudRate;
+
+            _comPort.Save();
+            _baudRate.Save();
+
+            Close();
+        }
+
+        private void ExecuteCancel()
+        {
+            _comPort.RejectChanges();
+            _baudRate.RejectChanges();
+
+            Close();
         }
     }
 }
